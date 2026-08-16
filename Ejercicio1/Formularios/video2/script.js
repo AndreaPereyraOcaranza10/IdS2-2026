f540cd053d788149ef1ce7e5f6b3e402a23405d7
AndreaPereyraOcaranza10 <andrea.a.pereyra02@gmail.com>
2026-08-16 20:46:47 -0300

    ejercicio1 formularios video2

---
+++ Ejercicio1/Formularios/video2/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const formulario = document.querySelector('.formulario')
+    const respuesta = document.querySelector('#parrafo')
+
+    respuesta.style.display = 'none'
+
+    formulario.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        const nombre = document.querySelector('#nombre').value
+        const password = document.querySelector('#password').value
+
+        if(nombre === '' || password === ''){
+
+            respuesta.style.display = 'block'
+            respuesta.textContent= 'Debe llenar todos los campos'
+            respuesta.style.backgroundColor = 'darkred'
+
+        } else{
+            respuesta.style.display = 'block'
+            respuesta.textContent = 'Formulario enviado'
+            respuesta.style.backgroundColor = 'darkgreen'
+
+        }
+    })
+})
